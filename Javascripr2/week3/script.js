8b6aa18fc4487b216ae334b83300f3aeae5144e6
Radhikasachin <sach.radhika@gmail.com>
2018-05-13 12:38:56 +0200

    Update script.js

--- Javascripr2/week3/script.js
+++ Javascripr2/week3/script.js
@@ -1,4 +1,35 @@
 
+//3.2 You must write a function that takes 4 arguments.
+/*A start value
+An end value
+A callback to call if the number is divisible by 3
+A callback to use if the number is divisible by 5
+The function should first generate an array containing values from start value to end value (inclusive).
+
+Then the function should take the newly created array and iterate over it, and calling the first callback if the array value is divisible by 3.
+
+The function should call the second callback if the array value is divisible by 5.
+
+Both functions should be called if the array value is divisible by both 3 and 5.*/
+
+
+console.log('\nStep 4.2');
+{
+  function threeFive(startIndex, stopIndex, threeCallback, fiveCallback) {
+    const numbers = [];
+    for (let i = startIndex; i <= stopIndex; i++) {
+      numbers.push(i);
+    }
+
+    numbers.forEach(num => {
+      if (num % 3 === 0) {
+        threeCallback(num);
+      }
+      if (num % 5 === 0) {
+        fiveCallback(num);
+      }
+    })
+  }
 
 
 
